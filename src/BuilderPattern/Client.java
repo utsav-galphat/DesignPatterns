e6d16071ea71d0ac4d6999b8d6f62549f1dcc1db
utsav-galphat <utsav.galphat97@gmail.com>
2023-06-27 22:12:24 +0530

    Builder pattern added

---
+++ src/BuilderPattern/Client.java
@@ -0,0 +1,23 @@
+package BuilderPattern;
+
+public class Client {
+    public static void main(String[] args) {
+        Student.Builder studentBuilder = Student.builder();
+        studentBuilder.setName("o");
+        studentBuilder.setAge(12);
+        studentBuilder.setBatchName("n");
+        studentBuilder.setPsp(9.9);
+
+        Student student2 = Student.builder().setPsp(3.3)
+                .setAge(1)
+                .setName("t")
+                .setBatchName("r")
+                .setPsp(2.3)
+                .build();
+
+
+        Student student = studentBuilder.build();
+
+        System.out.println(student2.toString());;
+    }
+}
